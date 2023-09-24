7fb2a533c1d7c3dea82909589f5e529009089a93
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-09-24 19:41:35 +0100

    edit scss

--- src/Components/Api/TopFive.jsx
+++ src/Components/Api/TopFive.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const TopFive = () => {
 
     const getData = async () => {
+        
+        async function fetchWebApi(endpoint, method, body) {
         // Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
         const token = 'BQA4Q9LdQTNZt9ueA65D3jt3lpYb6jCSj6YYH38YCGSmQwl48KNeicbo3EQX3xN2Cef6FNFtK_DyoE13IxjoPq-BxGYv5vH08Lxk5Q5kIzlZzmEhrYN0Er9EtaBx2TzMJ4c1V9noFGgFcTQOCdBaeB01haQDpbyzAyL7ZKXQD3ALZCrgrjaBROufKNUgiJ65b3hmQbf0XwhDRRy_vTK2PiApYWS5t2ZQB-WUDyMcRFdEWTtSWGlqsEb69w';
-        async function fetchWebApi(endpoint, method, body) {
         const res = await fetch(`https://api.spotify.com/${endpoint}`, {
             headers: {
             Authorization: `Bearer ${token}`,
--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='Footer'>Footer</div>
+    <div className='Footer'>
+      <h1>Footer</h1>
+    </div>
   )
 }
 
