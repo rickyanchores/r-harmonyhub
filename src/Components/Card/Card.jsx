e25942282ef219c5e07c368a41744009a96d4780
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-09-25 18:23:19 +0100

    added

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,6 +4,7 @@ const Card = () => {
   return (
     <div className='Card'>
         <h1>Card</h1>
+        <img src="https://images.unsplash.com/photo-1694873386852-baa93b5538f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80" alt="" />
         <p>This Card</p>
         <button className="btn">Link</button>
     </div>
