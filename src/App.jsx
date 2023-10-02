bd2cbe8e5e7327f60d68a61279519b66c6f679b4
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-10-02 18:18:06 +0100

    added genre component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './Sections/Home/Home'
 import Card from './Components/Card/Card'
 import TopFive from './Components/Api/TopFive'
 import Footer from './Sections/Footer/Footer'
+import Genre from './Components/Genre/Genre'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <p>powered by Spotify</p>
       <Home />
       <Card />
+      <Genre />
       <TopFive />
       <Footer />
     </div>
---
+++ src/Components/Genre/Genre.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Genre = () => {
+  return (
+    <div className='Genre'>
+        <h1>Genre</h1>
+        <p>Favorite Genre</p>
+        <li>Rock</li>
+        <li>Alternative</li>
+        <li>Pop</li>
+    </div>
+  )
+}
+
+export default Genre;
